b63e8218eeeb2b9a247702768fa8c798b9f242b1
rorodadu <162429925+malakhamydy@users.noreply.github.com>
2024-03-27 10:08:44 +0000

    main

---
+++ New folder/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Netflix Clone</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Bienvenue sur Netflix Clone</h1>
+    <a href="login.php">Connexion</a>
+    <a href="signup.php">Inscription</a>
+</body>
+</html>
---
+++ New folder/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Connexion - Netflix Clone</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Connexion</h1>
+    <form action="login_process.php" method="post">
+        <input type="text" name="username" placeholder="Nom d'utilisateur" required><br>
+        <input type="password" name="password" placeholder="Mot de passe" required><br>
+        <input type="submit" value="Se connecter">
+    </form>
+</body>
+</html>
